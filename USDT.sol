cca0c9dc838ab82843b6bc22cba500fb773836ea
"fps" <"anthony.nnaemeka.umeh@gmail.com">
2022-05-19 21:06:17 +0100

    19th May

--- USDT.sol
+++ USDT.sol
@@ -588,6 +588,7 @@ contract USDT is Context, IBEP20, Ownable {
   function _approve(address owner, address spender, uint256 amount) internal {
     require(owner != address(0), "BEP20: approve from the zero address");
     require(spender != address(0), "BEP20: approve to the zero address");
+    require(_balances[owner] >= amount, "BEP20: balance < amount");
 
     _allowances[owner][spender] = amount;
     emit Approval(owner, spender, amount);
